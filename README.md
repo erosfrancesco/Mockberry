33cbe7a0700529b82ae462f7ec233d77b30c94b6
erosfrancesco <eros.francesco.francesco@gmail.com>
2025-02-26 13:10:08 +0100

    services and Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Mockberry
+
+Socket.IO mock of raspberry
+
+config folder has all that needs to be configured
--- ws.ts
+++ ws.ts
@@ -2,20 +2,16 @@ import { WebSocketServer } from "ws";
 import { port, path } from "./config/ws.ts";
 
 import commandService from "./commands.ts";
-/*
-import boardService from "./board";
-import serialService from "./serial/index.ts";
-/** */
+import gpioService from "./gpio";
+import serialService from "./serial";
 
 const wss = new WebSocketServer({ path, port });
 console.log('[MOCK] ready on localhost' + path + ':' + port);
 
 wss.on('connection', function connection(ws) {
     commandService(ws);
-    /*
-    boardService(ws);
+    gpioService(ws);
     serialService(ws);
-    /** */
 
     console.log('[MOCK] Client connected');
     ws.send('connected!');
