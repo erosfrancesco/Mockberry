c51a48a0b531e98d70133f467430a0101ce01bc5
erosfrancesco <eros.francesco.francesco@gmail.com>
2025-10-03 12:49:06 +0200

    utils and tests

---
+++ tests.ts
@@ -0,0 +1,53 @@
+import { WebSocketServer } from "ws";
+import { port, path } from "./config/ws";
+import { Intervallable, Subscribable } from "./utils";
+
+// BE
+const initialValue = 1;
+const updateValue = (value: number) => {
+  return value + 1;
+};
+
+const clockPin = new Intervallable(
+  initialValue,
+  (value, sub) => {
+    console.log("[Notified]: " + sub + " - " + value);
+  },
+  1000,
+  updateValue
+);
+
+const commandShell = new Subscribable("", (value, sub) =>
+  console.log("[Notified]: Shell output: " + sub + " - " + value)
+);
+
+// FE
+const alarmWidget = {
+  id: "Alarm-Widget",
+};
+
+const clockWidget = {
+  id: "Clock-Widget",
+};
+
+const shellWidget = {
+  id: "Shell-Widget",
+  sendCommand: () => {
+    setTimeout(() => {
+      commandShell.value = "Folder: ./mock1.ts ./value.js";
+    }, 2000);
+  },
+};
+
+clockPin.subscribe(alarmWidget.id);
+clockPin.subscribe(clockWidget.id);
+commandShell.subscribe(shellWidget.id);
+
+// For now it needs only this functionalities. WS integration will take care of everything else.
+
+const wss = new WebSocketServer({ path, port });
+console.log("[MOCK] Test ready on localhost" + path + ":" + port);
+
+wss.on("connection", function connection() {
+  console.log("[MOCK] Test. Close this task yourself");
+});
--- utils.ts
+++ utils.ts
@@ -1,19 +1,16 @@
-import {
-  EventServiceType,
-  GpioServiceActions,
-  SerialServiceActions,
-} from "./config/ws";
+type ObservableCallback<T> = (value: T, ...args: any) => void;
 
+// Observer Functionality
 export class Observable<T> {
-  constructor(public initialValue: T, callback: Function = () => {}) {
+  constructor(initialValue: T, callback: ObservableCallback<T> = () => {}) {
     this.callback = callback;
     this.value = initialValue;
   }
 
-  callback: Function;
+  callback: ObservableCallback<T>;
   private _value!: T;
 
-  set value(v: T) {
+  set value(v) {
     this._value = v;
     if (this.callback) {
       this.callback(v);
@@ -25,11 +22,68 @@ export class Observable<T> {
   }
 }
 
-export interface Subscribable {
-  subscribed: Observable<Set<string>>;
+type SubscribableObservedCallback<T> = (value: T, id: string) => void;
+
+// PUB / SUB Functionality
+export class Subscribable<T> extends Observable<T> {
+  private subscribed;
+  private _callback;
+
+  constructor(
+    initialValue: T,
+    callback: SubscribableObservedCallback<T> = () => {}
+  ) {
+    super(initialValue);
+
+    this.subscribed = new Set<string>();
+    this._callback = callback;
+  }
+
+  subscribe(id: string) {
+    this.subscribed.add(id);
+    this.callback = (value) =>
+      this.subscribed.forEach((id) => this._callback(value, id));
+  }
+
+  unsubscribe(id: string) {
+    this.subscribed.delete(id);
+    this.callback = (value) =>
+      this.subscribed.forEach((id) => this._callback(value, id));
+  }
 }
 
-export interface Intervallable {
-  listening?: NodeJS.Timeout;
-  everyMs?: number;
+// Every ms update value and notify subs.
+export class Intervallable<T> extends Subscribable<T> {
+  constructor(
+    initialValue: T,
+    callback: SubscribableObservedCallback<T> = () => {},
+    everyMs: number = 2000,
+    update: (previous: T) => T = () => {
+      return initialValue;
+    }
+  ) {
+    super(initialValue, callback);
+    this._update = update;
+    this.everyMs = everyMs;
+  }
+
+  private _listening?: NodeJS.Timeout;
+  private _everyMs!: number;
+  private _update: (previous: T) => T;
+
+  get everyMs() {
+    return this._everyMs;
+  }
+
+  set everyMs(v) {
+    if (!v) {
+      clearInterval(this._listening);
+    }
+
+    this._everyMs = v;
+    this._listening = setInterval(() => {
+      const newValue = this._update(this.value);
+      this.value = newValue;
+    }, v);
+  }
 }
